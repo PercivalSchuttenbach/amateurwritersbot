6ea1442b3c7d3a8be8f829ec721ab5cb9ba9abbe
Glenn <glenn@glerum.nu>
2019-04-01 21:08:11 +0200

    testing

--- bot.js
+++ bot.js
@@ -18,6 +18,10 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+    bot.sendMessage({
+        to: channelID,
+        message: 'test!'
+    });
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 18) == '@AmateurWritersBot') {
