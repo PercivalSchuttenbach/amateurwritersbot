5f1f3373acb558f02fcd79a4a56da55e72641edf
Glenn <glenn@glerum.nu>
2019-04-01 21:32:29 +0200

    wip

--- bot.js
+++ bot.js
@@ -19,10 +19,7 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
 
-    bot.sendMessage({
-        to: channelID,
-        message: bot.guilds
-    });
+    logger.info( bot.guilds );
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
