2b83fe4c818df67f112a7add78c45b61366dba0a
Glenn <glenn@glerum.nu>
2019-04-03 10:25:37 +0200

    fixing bugs and expanding features

--- bot.js
+++ bot.js
@@ -8,48 +8,55 @@ var memory = {
         {
             'timestamp': null,
             'users': {}
+        },
+        'writing':
+        {
+            'start': null,
+            'cooldown': null,
+            'count': 0
         }
     }
 };
 
 function krewlGate(user, channelID, currentTime){
-    if(!memory['AmateurWritersBot'].krewlgate.timestamp || (currentTime - memory['AmateurWritersBot'].krewlgate.timestamp) >= 3600000){
+    var krewlgateMemory = memory['AmateurWritersBot'].krewlgate;
+    if(!krewlgateMemory.timestamp || (currentTime - krewlgateMemory.timestamp) >= 3600000){
         bot.sendMessage({
             to: channelID,
             message: 'https://cdn.discordapp.com/attachments/522773675263655983/561929502087970818/unknown.png'
         });
-        memory['AmateurWritersBot'].krewlgate.timestamp = +new Date();
-        memory['AmateurWritersBot'].krewlgate.users = {};
+        krewlgateMemory.timestamp = +new Date();
+        krewlgateMemory.users = {};
     }
     else
     {
         //set user count to 0
-        if(!memory['AmateurWritersBot'].krewlgate.users[user]){
-            memory['AmateurWritersBot'].krewlgate.users[user] = 0;
+        if(!krewlgateMemory.users[user]){
+            krewlgateMemory.users[user] = 0;
         }
         //if the user used the command 7 times, reset
-        if(memory['AmateurWritersBot'].krewlgate.users[user]==7){
-            memory['AmateurWritersBot'].krewlgate.users[user] = 1;
+        if(krewlgateMemory.users[user]==7){
+            krewlgateMemory.users[user] = 0;
         }
-        memory['AmateurWritersBot'].krewlgate.users[user]++;
+        krewlgateMemory.users[user]++;
         //call the user out for being an ass
-        switch(memory['AmateurWritersBot'].krewlgate.users[user]){
+        switch(krewlgateMemory.users[user]){
             case 1:
                 bot.sendMessage({
                     to: channelID,
                     message: user + ' stop harrasing the poor boy.'
                 });
             break;
-            case 3:
+            case 2:
                 bot.sendMessage({
                     to: channelID,
                     message: user + ' you deaf?'
                 });
             break;
-            case 5:
+            case 3:
                 bot.sendMessage({
                     to: channelID,
-                    message: user + ' is an asshat!'
+                    message: 'Mom ' + user + ' is picking on Krewl again!'
                 });
             break;
         }
@@ -75,6 +82,44 @@ function getRandomCritiqueUser(){
     return user;
 }
 
+//have it check witihin the scope of 15 minutes for to much talk about writing
+function writingCallOut(user, message, currentTime, channelID){
+    logger.info('Writing callout check');
+    //when writing or write is detected run. Writing or Write should be at the start of the word
+    if(message.toLowerCase().search('\\bwriting') > -1 || message.toLowerCase().search('\\bwrite') > -1){
+        //check if memory storage is available
+        if(!memory['AmateurWritersBot']['writing']){
+            memory['AmateurWritersBot']['writing'] = { start: null, count: 0, cooldown: null };
+            logger.info('Created memory store');
+        }
+        var writingMemory = memory['AmateurWritersBot']['writing'];
+        //make sure it's non-obtrusive, run once every hour
+        if( !writingMemory.cooldown || (currentTime >= writingMemory.cooldown) ){
+            //reset start and counter when banter is not resumed within 15 minutes
+            if( !writingMemory.start || (currentTime - writingMemory.start) > (60 * 15 * 1000) ){
+                writingMemory.count = 0;
+                writingMemory.start = currentTime;
+            }
+
+            //keep incrementing if not yet counted banter 4 times
+            if(writingMemory.count != 4){
+                writingMemory.count++;
+                logger.info('Incremented writing for user: ' + user);
+            }else{
+                logger.info('All talk');
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'All you guys talk about is writing. When are you going to start? :thinking: '
+                });
+
+                // make sure it's not fired again for at least an hour
+                writingMemory.cooldown = currentTime + (60 * 60 * 1000);
+                writingMemory.count = 0;
+            }
+        }
+    }
+}
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -94,64 +139,71 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     var currentTime = +new Date();
 
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '~') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            case 'random':
-                var userChannel = getRandomCritiqueUser();
-                bot.sendMessage({
-                    to: channelID,
-                    message: user + ' your random channel to critique is #' + userChannel + '\ndon\'t forget to mention the corresponding user in #feedback'
-                });
-            break;
-            case 'krewlgate':
-                krewlGate(user, channelID, currentTime);
-            break;
-         }
-     }
-     else{
-        //522773675263655983
-        if(channelID=='522773675263655983' && user != 'AmateurWritersBot'){
-            logger.info('In channel. Yep.');
-            //@WRITING
-            if(message.toLowerCase().indexOf(' writing ') > -1 || message.toLowerCase().indexOf(' write ') > -1){
-                if(!memory[user]){
-                    memory[user] = { 'writing': {timestamp: null, count: 0} };
-                    logger.info('Creating user: ' + user);
-                }
-                if(!memory[user]['writing']){
-                    memory[user]['writing'] = {timestamp: null, count: 0};
-                    logger.info('Adding writing to user: ' + user);
-                }
-                if(!memory[user]['writing'].timestamp || (currentTime - memory[user]['writing'].timestamp) >= 3600000){
-                    if(memory[user].writing.count != 5){
-                        memory[user].writing.count++;
-                        logger.info('Incremented writing for user: ' + user);
-                    }else{
-                        logger.info('Message writing for user: ' + user);
-                        bot.sendMessage({
-                            to: channelID,
-                            message: user + ' writing... writing... all you talk about is writing. When are you going to start :thinking: '
-                        });
-
-                        // make sure it's not fired again for at least an hour
-                        memory[user].writing.timestamp = +new Date();
-                        memory[user].writing.count = 0;
-                    }
-                }
-            }
+    //testing
+    if(channelID=='562335386970357779' && user != 'AmateurWritersBot'){
+         // Our bot needs to know if it will execute a command
+        // It will listen for messages that will start with `!`
+        if (message.substring(0, 1) == '~') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+           
+            args = args.splice(1);
+            switch(cmd) {
+                // !ping
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Pong!'
+                    });
+                break;
+                case 'random':
+                    var userChannel = getRandomCritiqueUser();
+                    bot.sendMessage({
+                        to: channelID,
+                        message: user + ' your random channel to critique is #' + userChannel + '\ndon\'t forget to mention the corresponding user in #feedback'
+                    });
+                break;
+                case 'krewlgate':
+                    krewlGate(user, channelID, currentTime);
+                break;
+             }
         }
+        else {
+            writingCallOut(user, message, currentTime, channelID);
+        }
+    }
+
+    if(channelID=='522773675263655983' && user != 'AmateurWritersBot'){
+        // Our bot needs to know if it will execute a command
+        // It will listen for messages that will start with `!`
+        if (message.substring(0, 1) == '~') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+           
+            args = args.splice(1);
+            switch(cmd) {
+                // !ping
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Pong!'
+                    });
+                break;
+                case 'random':
+                    var userChannel = getRandomCritiqueUser();
+                    bot.sendMessage({
+                        to: channelID,
+                        message: user + ' your random channel to critique is #' + userChannel + '\ndon\'t forget to mention the corresponding user in #feedback'
+                    });
+                break;
+                case 'krewlgate':
+                    //krewlGate(user, channelID, currentTime);
+                break;
+             }
+         }
+         else{
+            //522773675263655983
+            writingCallOut(user, message, currentTime, channelID);
+         }
      }
 });
