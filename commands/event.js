f29d5c12a17dac72bcd03555e5e5e5bb1b32f437
Glenn <glenn@glerum.nu>
2020-05-12 19:52:19 +0200

    creating aliases for narrate and banter

--- commands/event.js
+++ commands/event.js
@@ -225,7 +225,7 @@ class Event
         }
         //execute subcommand method. On error catch and send to channel
         //@todo remove all data on exception
-        await this[command](args, message).catch(this.handleError.bind(this));
+        await this[command](message, args).catch(this.handleError.bind(this));
         this.silent = false;
         return;
     }
@@ -256,7 +256,7 @@ class Event
     /**
     * Start the event that has been set
     */
-    async start(args, message)
+    async start(message, args)
     {
         this.isNotRunning();
 
@@ -541,7 +541,7 @@ class Event
     * @param array
     * @param Message
     */
-    async type(args, message)
+    async type(message, args)
     {
         this.isRunning();
 
@@ -606,7 +606,7 @@ class Event
     * Run premade Event from DUNGEONS list
     * @param array
     */
-    async dungeon(args, message)
+    async dungeon(message, args)
     {
         this.isNotRunning();
 
@@ -633,7 +633,7 @@ class Event
     *
     * @param array
     **/
-    async set(args, message)
+    async set(message, args)
     {
         if (this.running || this.settingUp) {
             throw `An event is already running or being setup.`;
@@ -870,14 +870,22 @@ class Event
 
     }
 
+    hasAccess(command, message)
+    {
+        this.isRunning().catch(err => this.sendFeedbackToChannel(err, true));
+        if (!SUB_COMMANDS['narrate'](message)) return this.sendFeedbackToChannel('No permission to use this feature.', true);
+    }
+
     /**
      *  Send message as the narrator to the sprint channel
      *
      * @param array args
      * @param Message message
      */
-    async narrate(args, message)
+    async narrate(message)
     {
+        this.hasAccess('narrate', message);
+
         const content = message.content.replace(/^~event\snarrate\s/, '');
         const embed = new this.Discord.MessageEmbed()
             .setDescription(content)
@@ -892,8 +900,10 @@ class Event
     * @param array args
     * @param Message message
     */
-    async banter(args, message)
+    async banter(message)
     {
+        this.hasAccess('banter', message);
+
         const { name, thumbnail } = this.getCurrentEnemy();
 
         const content = message.content.replace(/^~event\sbanter\s/, '');
