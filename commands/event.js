6d7bf8d4806b8b5b63d6d9598461696d38a9d282
Glenn <glenn@glerum.nu>
2020-07-02 19:51:22 +0200

    fixed wc submission listeners

--- commands/event.js
+++ commands/event.js
@@ -302,8 +302,8 @@ class Event
         {
             this.listeners.start.push(this.addListener(start_text, true, this.sprintInitiated));
             this.listeners.cancel.push(this.addListener(cancel, true, this.sprintCanceled));
-            this.listeners.col.push(this.addListener(collect_start, false, this.listenForWc));
-            this.listeners.stop.push(this.addListener(collect_stop, false, this.sumbitSprintWc));
+            this.listeners.col.push(this.addListener(collect_start, true, this.listenForWc));
+            this.listeners.stop.push(this.addListener(collect_stop, true, this.sumbitSprintWc));
             this.listeners.writing.push(this.addListener(writing, true, this.sprintBegins));
             this.listeners.join.push(this.addListener(join, false, this.joinSprint));
             this.listeners.leave.push(this.addListener(leave, false, this.leaveSprint));
