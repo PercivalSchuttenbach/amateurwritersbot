b99c5d222c66761575124ea1f0568085b66b14c0
Glenn <glenn@glerum.nu>
2021-02-11 15:50:57 +0100

    fixing bugs

--- app/reactions.js
+++ app/reactions.js
@@ -24,11 +24,13 @@ class Reactions
         }
         if (messageReaction && messageReaction.emoji.id === BONK_EMOJI && messageReaction.count >= BONKS) {
             //messageReaction.message.member.roles.add(this.bonkRole);
-            const member = await messageReaction.message.guild.members.fetch(messageReaction.message.author);
+            const guildMember = await messageReaction.message.guild.members.fetch(messageReaction.message.author);
+            if (this.jail.find(({ member }) => member.id === guildMember.id)) return;
             try {
                 member.roles.add(this.bonkRole);
-                this.jail.push({ member, timestamp: Date.now() + TIMEOUT });
+                this.jail.push({ member: guildMember, timestamp: Date.now() + TIMEOUT });
                 this.startTicker();
+                messageReaction.remove();
             } catch (e) { console.log(e); }
         }
     }
@@ -40,7 +42,7 @@ class Reactions
 
     checkJail()
     {
-        this.jail.forEach(({ member, timestamp }, index, array) =>
+        this.jail.forEach(({ member, message, timestamp }, index, array) =>
         {
             if (Date.now() > timestamp) {
                 member.roles.remove(this.bonkRole);
